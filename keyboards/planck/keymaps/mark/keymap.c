b80255cce85b27d3ed568e9d752b3c7ec037c104
Mark Fletcher <markpfletcher@mac.com>
2017-09-22 09:58:49 +0100

    Reverted enter key position

--- keyboards/planck/keymaps/mark/keymap.c
+++ keyboards/planck/keymaps/mark/keymap.c
@@ -3,10 +3,13 @@
 
 #include "planck.h"
 #include "action_layer.h"
+#ifdef AUDIO_ENABLE
+  #include "audio.h"
+#endif
 #ifdef BACKLIGHT_ENABLE
   #include "backlight.h"
 #endif
-#include "eeconfig.h"
+//#include "eeconfig.h"
 
 extern keymap_config_t keymap_config;
 
@@ -57,6 +60,8 @@ enum planck_keycodes {
   EXT_NUM
 };
 
+#define SFT_CAP TD(0)	// Left shift, double tap for caps
+
 // Fillers to make layering more clear
 #define _______ KC_TRNS
 #define XXXXXXX KC_NO
@@ -76,8 +81,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_QWERTY] = { /* Qwerty */
   {KC_TAB,       KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSPC},
-  {KC_ESC,       KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT},
-  {KC_LSFT,      KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  F(0)},
+  {KC_ESC,       KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_ENT},
+  {SFT_CAP,      KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  F(0)},
   {MOVE,         KC_LCTL,  KC_LALT,  KC_LGUI,  LOWER,    KC_SPC,   KC_SPC,   RAISE,    KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT}
 },
 
@@ -175,8 +180,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 };
 
+// Tap Dance Definitions
+qk_tap_dance_action_t tap_dance_actions[] = {
+	[0]  = ACTION_TAP_DANCE_DOUBLE(KC_LSFT, KC_CAPS)
+};
+
 const uint16_t PROGMEM fn_actions[] = {
-  [0]  = ACTION_MODS_TAP_KEY(MOD_RSFT, KC_ENT),
+  [0]  = ACTION_MODS_TAP_KEY(MOD_RSFT, KC_QUOT),
 };
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
